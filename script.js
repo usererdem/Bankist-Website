c0b927dc23e38475de44d6866e6040c229251c89
Erdem Elvan <erdemelvan35@gmail.com>
2022-09-12 19:47:41 +0300

    mobile navigation links and logo, closes the navigation menu on click createAccount button, openModal function, setTimeout added for wait navigation menu close animation

--- script.js
+++ script.js
@@ -9,8 +9,15 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  if (window.innerWidth <= 900) {
+    setTimeout(() => {
+      modal.classList.remove('hidden');
+      overlay.classList.remove('hidden');
+    }, 750);
+  } else {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  }
 };
 
 const closeModal = function () {
@@ -28,4 +35,19 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+///////////////////////////////////////
+// Mobile Navigation
+const navLink = document.querySelector('.nav__links');
+const navLogo = document.querySelector('.nav__logo');
+
+const navButton = document.querySelector('.navigation__button');
+const checkbox = document.getElementById('navi-toggle');
+
+navLink.addEventListener('click', function () {
+  checkbox.checked = !checkbox.checked;
+});
+navLogo.addEventListener('click', function () {
+  checkbox.checked = !checkbox.checked;
 });
