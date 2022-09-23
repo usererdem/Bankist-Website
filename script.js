050f79634f5608199aa891b30581778630aca784
Erdem Elvan <erdemelvan35@gmail.com>
2022-09-23 14:28:46 +0300

    Bug fixed, navlink pointer events are none in mobile window When user opens the nav menu pointer event class is deleted so, pointer event sets to auto

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 'use strict';
-
 ///////////////////////////////////////
+//////////// Elements
 // Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+// Mobile Navigation
+const navLink = document.querySelector('.nav__links');
+const navLogo = document.querySelector('.nav__logo');
 
+const navButton = document.querySelector('.navigation__button');
+const checkbox = document.getElementById('navi-toggle');
+
+// Scrolling Animation
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+//////////// Functions
+// Modal window
 const openModal = function (e) {
   e.preventDefault();
   if (window.innerWidth <= 900) {
@@ -26,6 +38,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
+///////////////////////////////////////
+//////////// Event Listeners
+// Modal window
 btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
@@ -37,17 +52,25 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-///////////////////////////////////////
 // Mobile Navigation
-const navLink = document.querySelector('.nav__links');
-const navLogo = document.querySelector('.nav__logo');
-
-const navButton = document.querySelector('.navigation__button');
-const checkbox = document.getElementById('navi-toggle');
+if (window.innerWidth <= 900) {
+  navLink.classList.add('pointer-event');
+  navLogo.classList.add('pointer-event');
+}
+navButton.addEventListener('click', function () {
+  navLink.classList.toggle('pointer-event');
+  navLogo.classList.toggle('pointer-event');
+});
 
 navLink.addEventListener('click', function () {
   checkbox.checked = !checkbox.checked;
 });
 navLogo.addEventListener('click', function () {
   checkbox.checked = !checkbox.checked;
+});
+
+// Scrolling Animation
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
 });
