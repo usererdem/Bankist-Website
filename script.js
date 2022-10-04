154bf324e8db7269860aa6165aa9a0eb463b973d
Erdem Elvan <erdemelvan35@gmail.com>
2022-10-04 13:15:02 +0300

    1. Operations Section functionality is done 2. Navigation Menu Fade animation created on mouse hover

--- script.js
+++ script.js
@@ -21,6 +21,11 @@ const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const header = document.querySelector('#header');
 
+//// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
 ///////////////////////////////////////
 //////////// Functions
 //// Modal window
@@ -67,7 +72,7 @@ document.addEventListener('keydown', function (e) {
 //// Mobile Navigation
 // Adding pointer-event:none as default to all links and to logo in mobile
 if (window.innerWidth <= 900) {
-  navBtnTogglePointerEvents()
+  navBtnTogglePointerEvents();
 
   // Toggling pointer-event between: (auto-none)
   navButton.addEventListener('click', navBtnTogglePointerEvents);
@@ -145,3 +150,68 @@ navLinks1.addEventListener('click', function (e) {
 navLogoLink.addEventListener('click', function (e) {
   header.scrollIntoView({ behavior: 'smooth' });
 });
+
+//// Tabbed component
+// tabs.forEach(t => t.addEventListener('click', () => console.log('Tabl')));
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Active tab
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  tabsContent.forEach(content =>
+    content.classList.remove('operations__content--active')
+  );
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+// Navigation Menu fade animation
+// With Closure
+function handleHover(x) {
+  return function (e) {
+    if (e.target.classList.contains('nav__link')) {
+      const link = e.target;
+      const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+      const logo = link.closest('.nav').querySelector('img');
+
+      siblings.forEach(el => {
+        if (el !== link) {
+          el.style.opacity = x;
+        }
+      });
+      logo.style.opacity = x;
+    }
+  };
+}
+nav.addEventListener('mouseover', handleHover(0.5));
+
+nav.addEventListener('mouseout', handleHover(1));
+// With bind method
+/* function handleHover(e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) {
+        el.style.opacity = this;
+      }
+    });
+    logo.style.opacity = this;
+  }
+}
+
+// Passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1)); */
