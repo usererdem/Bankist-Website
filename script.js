98d8dc859eb91fd015839173f02dadffb7fbe8cc
Erdem Elvan <erdemelvan35@gmail.com>
2022-10-06 19:01:00 +0300

    Lazy loading images added on the features section with the help of intersectionObserver API

--- script.js
+++ script.js
@@ -252,7 +252,6 @@ const navHeight2 = navigation.getBoundingClientRect().height;
 
 function stickyNav(entries) {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
@@ -269,7 +268,6 @@ const allSections = document.querySelectorAll('.section');
 
 function revealSection(entries, observer) {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) return;
 
@@ -286,3 +284,29 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// Lazy Loading Images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+function loadImg(entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px'
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
